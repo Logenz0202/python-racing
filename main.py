6e0e2cdcc9947dbe51e054072bb8e8133c977b80
Szymon Ligenza <szymek.ligenza@gmail.com>
2024-01-10 19:23:08 +0100

    base setting

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-# Szymon Ligenza
+import pygame
+from pygame.locals import *
+import random
 
-print("Hello World!")
+pygame.init()
+
+# window
+width = 500
+height = 500
+window_size = (width, height)
+window = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Pro Racer")
+
+# colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+grey = (128, 128, 128)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow =(255, 255, 0)
